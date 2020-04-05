1b0324129a0c99d3959f8b897fbab2db24d7638e
alexander-braun <alexanderbraun@freenet.de>
2020-04-05 21:45:24 +0200

    Updates

---
+++ precache-manifest.fab57f7639d288b8eb94618bd332a1e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bffcd56ab2da4326cda375c7b7debaa",
+    "url": "/redux-chat-app/index.html"
+  },
+  {
+    "revision": "53ba8a26fa2a2d376cd1",
+    "url": "/redux-chat-app/static/css/main.56d6b750.chunk.css"
+  },
+  {
+    "revision": "4b428bfd13eb15e339d4",
+    "url": "/redux-chat-app/static/js/2.f080394f.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/redux-chat-app/static/js/2.f080394f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ba8a26fa2a2d376cd1",
+    "url": "/redux-chat-app/static/js/main.84cc8bae.chunk.js"
+  },
+  {
+    "revision": "7ce625fffd4512ff3c35",
+    "url": "/redux-chat-app/static/js/runtime-main.ea59e753.js"
+  }
+]);
