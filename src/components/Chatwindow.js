ad17cd2f31f2893d38597518648082869269c755
alexander-braun <alexanderbraun@freenet.de>
2020-04-05 21:11:01 +0200

    finalized

--- src/components/Chatwindow.js
+++ src/components/Chatwindow.js
@@ -3,9 +3,11 @@ import store from '../store'
 import Header from '../components/Header'
 import Chats from '../components/Chats'
 import './Chatwindow.css'
+import MessageInput from './MessageInput'
 
 const ChatWindow = ({ activeUserId }) => {
     const state = store.getState()
+    const { typing } = state
     const activeUser = state.contacts[activeUserId]
     const activeMsgs = state.messages[activeUserId]
 
@@ -13,6 +15,7 @@ const ChatWindow = ({ activeUserId }) => {
         <div className="ChatWindow">
             <Header user={activeUser} />
             <Chats messages={Object.values(activeMsgs)}/>
+            <MessageInput value={typing} />
         </div>
     )
 }
--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,5 +1,25 @@
 import { getMessages } from '../static-data'
+import { SEND_MESSAGE } from '../constants/action-types'
 
 export default function messages(state = getMessages(10), action) {
-    return state
+    switch(action.type) {
+        case SEND_MESSAGE:
+            const { message, userId } = action.payload
+            const allUserMsg = state[userId]
+            const number = Object.keys(allUserMsg).pop() + 1
+
+            return {
+                ...state,
+                [userId]: {
+                    ...allUserMsg,
+                    [number]: {
+                        number,
+                        text: message,
+                        is_user_msg: true
+                    }
+                }
+            }
+        default:
+            return state
+    }
 }
